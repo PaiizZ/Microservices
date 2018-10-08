ef12a4e5c1980333f7b19948cae31e69f6404156
PaiizZ <wanchanapon.t@ku.th>
2018-10-08 11:04:35 +0700

    create eurekaservice project

---
+++ EmployeeEurekaServer/src/main/java/com/example/EmployeeEurekaServer/EmployeeEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.EmployeeEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeEurekaServerApplication.class, args);
+	}
+}
