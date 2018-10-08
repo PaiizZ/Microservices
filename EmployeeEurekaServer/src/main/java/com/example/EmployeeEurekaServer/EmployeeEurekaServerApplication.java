77e218dad088a7abd6dcbdf6d9e41eb6abe5911b
PaiizZ <wanchanapon.t@ku.th>
2018-10-08 11:19:12 +0700

    set up eureka service

--- EmployeeEurekaServer/src/main/java/com/example/EmployeeEurekaServer/EmployeeEurekaServerApplication.java
+++ EmployeeEurekaServer/src/main/java/com/example/EmployeeEurekaServer/EmployeeEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.example.EmployeeEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EmployeeEurekaServerApplication {
 
