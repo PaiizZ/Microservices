3896555e9718fe6b74db9e4b712411ac4f85c54f
PaiizZ <wanchanapon.t@ku.th>
2018-10-08 17:41:06 +0700

    add dockerfile and docker-compose

--- EmployeeDashBoardService/src/main/java/com/example/EmployeeDashBoardService/EmployeeDashBoardServiceApplication.java
+++ EmployeeDashBoardService/src/main/java/com/example/EmployeeDashBoardService/EmployeeDashBoardServiceApplication.java
@@ -3,12 +3,10 @@ package com.example.EmployeeDashBoardService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
@@ -18,6 +16,8 @@ public class EmployeeDashBoardServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeDashBoardServiceApplication.class, args);
 	}
+
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
