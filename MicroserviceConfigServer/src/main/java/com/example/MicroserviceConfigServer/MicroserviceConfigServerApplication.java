dcf590ca2802dc29b4416783e520b73f3f740de8
PaiizZ <wanchanapon.t@ku.th>
2018-10-08 10:46:02 +0700

    Create configserver project

---
+++ MicroserviceConfigServer/src/main/java/com/example/MicroserviceConfigServer/MicroserviceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.MicroserviceConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+	}
+}
